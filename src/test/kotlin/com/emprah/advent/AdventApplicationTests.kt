3f83ca9908948a96c146ec94d96dbc886fbbbc21
Lukas M <lukas_mueller_web@web.de>
2023-12-23 22:13:56 +0100

    spring initializr

---
+++ src/test/kotlin/com/emprah/advent/AdventApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.emprah.advent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdventApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
