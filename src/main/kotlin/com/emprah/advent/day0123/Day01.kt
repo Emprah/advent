dfcc15f30d38a27829b1470c0c1085e0712d987e
Lukas M <lukas_mueller_web@web.de>
2023-12-24 00:47:52 +0100

    Implements day 01 solution

--- src/main/kotlin/com/emprah/advent/day0123/Day01.kt
+++ src/main/kotlin/com/emprah/advent/day0123/Day01.kt
@@ -1,9 +1,29 @@
 package com.emprah.advent.day0123
 
 import com.emprah.advent.Puzzle
+import com.emprah.advent.util.FileService
 import org.springframework.stereotype.Component
 
 @Component
-class Day01 : Puzzle() {
+internal class Day01(
+    private val fileService: FileService,
+    private val calculator: CalibrationCalculator
+) : Puzzle() {
 
+    private val inputFile = "input.txt"
+    private val dayPath = "23/day01/"
+
+    override fun solve() {
+        val fileContent = fileService
+            .getFile(inputFile, dayPath)
+            ?.bufferedReader()?.readLines()
+
+        if (fileContent == null) {
+            println("Input file could not be read")
+            return
+        }
+
+        val calibrationSum = calculator.calculateCalibration(fileContent)
+        println("Calibration sum is $calibrationSum")
+    }
 }
