91634085277b42c617c36dbf8237fe2c08f0ae38
Lukas M <lukas_mueller_web@web.de>
2023-12-26 21:39:30 +0100

    Extracts solution calculation of day 03 part 01 to EngineCalculator

--- src/main/kotlin/com/emprah/advent/day0323/Day03Part01.kt
+++ src/main/kotlin/com/emprah/advent/day0323/Day03Part01.kt
@@ -11,6 +11,7 @@ private val logger = KotlinLogging.logger {}
 @Component(taskId)
 class Day03Part01 (
     private val fileService: FileService,
+    private val calculator: EngineCalculator
 ) : Puzzle() {
 
     private val inputFile = "input.txt"
@@ -27,64 +28,9 @@ class Day03Part01 (
             return
         }
 
-        val sumOfPartNumbers = getSumOfValidPartNumbers(fileContent)
+        val sumOfPartNumbers = calculator.getSumOfValidPartNumbers(fileContent)
         logger.info { "Sum of all valid part numbers equals $sumOfPartNumbers" }
     }
 
-    private fun getSumOfValidPartNumbers(fileContent: List<String>): Int {
-        val (partNumbers, symbols) = parseEngineSpec(fileContent)
 
-        return partNumbers
-            .filter { isValidPart(it, symbols) }
-            .sumOf { it.value }
-    }
-
-    private fun parseEngineSpec(input: List<String>): EngineSpec {
-        val partNumbers = mutableListOf<PartNumber>()
-        val symbols = mutableListOf<Symbol>()
-
-        for ((lineNumber, line) in input.withIndex()) {
-            for (numberMatch in line.matchesOf("""\d+""")) {
-                partNumbers.add(
-                    PartNumber(numberMatch.value.toInt(10), lineNumber, numberMatch.range.first))
-            }
-            for (symbolMatch in line.matchesOf("""[^\d.]""")) {
-                symbols.add(
-                    Symbol(symbolMatch.value[0], lineNumber, symbolMatch.range.first))
-            }
-        }
-
-        return EngineSpec(partNumbers, symbols)
-    }
-
-    private fun String.matchesOf(pattern: String): Sequence<MatchResult> {
-        val regex = pattern.toRegex()
-        return regex.findAll(this)
-    }
-
-    private fun isValidPart(part: PartNumber, symbols: List<Symbol>): Boolean {
-        return symbols
-            .filter { it.line == part.line - 1 || it.line == part.line || it.line == part.line + 1 }
-            .filter { part.column - 1 <= it.column && it.column <= part.lastColumn + 1 }
-            .isNotEmpty()
-    }
 }
-
-private data class EngineSpec (
-    val partNumbers: List<PartNumber>,
-    val symbols: List<Symbol>,
-)
-
-private data class PartNumber (
-    val value: Int,
-    val line: Int,
-    val column: Int,
-) {
-    val lastColumn = column + value.toString().length - 1
-}
-
-private data class Symbol(
-    val value: Char,
-    val line: Int,
-    val column: Int,
-)
---
+++ src/main/kotlin/com/emprah/advent/day0323/EngineCalculator.kt
@@ -0,0 +1,64 @@
+package com.emprah.advent.day0323
+
+import org.springframework.stereotype.Service
+
+@Service
+object EngineCalculator {
+
+    fun getSumOfValidPartNumbers(fileContent: List<String>): Int {
+        val (partNumbers, symbols) = parseEngineSpec(fileContent)
+
+        return partNumbers
+            .filter { isValidPart(it, symbols) }
+            .sumOf { it.value }
+    }
+
+    private fun parseEngineSpec(input: List<String>): EngineSpec {
+        val partNumbers = mutableListOf<PartNumber>()
+        val symbols = mutableListOf<Symbol>()
+
+        for ((lineNumber, line) in input.withIndex()) {
+            for (numberMatch in line.matchesOf("""\d+""")) {
+                partNumbers.add(
+                    PartNumber(numberMatch.value.toInt(10), lineNumber, numberMatch.range.first))
+            }
+            for (symbolMatch in line.matchesOf("""[^\d.]""")) {
+                symbols.add(
+                    Symbol(symbolMatch.value[0], lineNumber, symbolMatch.range.first))
+            }
+        }
+
+        return EngineSpec(partNumbers, symbols)
+    }
+
+    private fun String.matchesOf(pattern: String): Sequence<MatchResult> {
+        val regex = pattern.toRegex()
+        return regex.findAll(this)
+    }
+
+    private fun isValidPart(part: PartNumber, symbols: List<Symbol>): Boolean {
+        return symbols
+            .filter { it.line == part.line - 1 || it.line == part.line || it.line == part.line + 1 }
+            .filter { part.column - 1 <= it.column && it.column <= part.lastColumn + 1 }
+            .isNotEmpty()
+    }
+}
+
+private data class EngineSpec (
+    val partNumbers: List<PartNumber>,
+    val symbols: List<Symbol>,
+)
+
+private data class PartNumber (
+    val value: Int,
+    val line: Int,
+    val column: Int,
+) {
+    val lastColumn = column + value.toString().length - 1
+}
+
+private data class Symbol(
+    val value: Char,
+    val line: Int,
+    val column: Int,
+)
